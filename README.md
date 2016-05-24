e103d88975e4b75fcee1762dd17b45ded9129348
3846masa <3846masahiro+git@gmail.com>
2016-05-24 16:15:00 +0900

    Init

---
+++ README.md
@@ -0,0 +1,23 @@
+SMTP2SLACK
+==========
+
+Inspired by [SRARAD/slack-email].
+
+[SRARAD/slack-email]: https://github.com/SRARAD/slack-email
+
+## Usage
+
+1. Set MX record
+1. Install docker-compose
+1. Edit ``SLACK_TOKEN`` in docker-compose.yml
+1. ``docker-compose up -d``
+1. Forward an email to ``channelname@yourdomain.example.com``
+
+## Bot icon
+
+For example, you will set ``:slack:`` icon to bot, you forward to ``channelname+slack@yourdomain.example.com``.
+
+If you don't set icon name, bot talk with ``:email:`` icon.
+
+## LICENSE
+(c) 3846masa MIT
---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const mailin = require('mailin')
+const Slack = require('@slack/client')
+const config = require('config')
+
+const slack = new Slack.WebClient(config.get('slack.token'))
+
+mailin.start({
+  port: 25,
+  disableWebhook: true
+})
+
+mailin.on('authorizeUser', () => {
+  done(new Error('Unauthorized'), false)
+})
+
+mailin.on('message', (conn, msg) => {
+  console.dir(msg, { depth: null })
+
+  const sentConfs =
+    msg.envelopeTo.map((i) => {
+      const local = i.address.split('@')[0]
+      const [ channel, icon ] = local.split('+')
+      return { channel, icon }
+    })
+
+  const postDataDefault = {
+    username: 'Mailbot',
+    icon_emoji: ':email:',
+    attachments: [{
+      fallback: msg.subject,
+      title: msg.subject,
+      author_name: msg.from[0].address,
+      text: msg.text
+    }]
+  }
+
+  sentConfs.forEach((conf) => {
+    const postData = Object.assign({}, postDataDefault, {
+      icon_emoji: (conf.icon) ? `:${ conf.icon }:` : ':email:'
+    })
+    slack.chat.postMessage(`#${ conf.channel }`, '', postData)
+  })
+})
